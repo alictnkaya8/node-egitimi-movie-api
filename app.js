30a528cf918fde0757912b7b503ebc446c47db64
alictnkaya8 <49043825+alictnkaya8@users.noreply.github.com>
2022-08-10 11:30:34 +0300

    movie model added

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser')
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const movieRouter = require('./routes/movie');
 
 const app = express();
 
 // DB connection
-
 const db = require('./helper/db.js')();
 
 // view engine setup
@@ -18,13 +18,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/movie', movieRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
