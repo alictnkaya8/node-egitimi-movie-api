b34f2fbd8050d3d9dbf1defbea3a869212a28d15
alictnkaya8 <49043825+alictnkaya8@users.noreply.github.com>
2022-08-10 15:45:42 +0300

    get directors by director id method added

--- routes/director.js
+++ routes/director.js
@@ -1,8 +1,10 @@
+const mongoose = require('mongoose')
 const express = require('express');
 const router = express.Router();
 
 // Models
-const Director = require('../models/Director')
+const Director = require('../models/Director');
+const { route } = require('./movie');
 
 router.post('/', (req, res, next) => {
   const director = new Director(req.body)
@@ -47,7 +49,7 @@ router.get('/', (req, res) => {
     {
       $project: {
         _id: '$_id._id',
-        name: '_id.name',
+        name: '$_id.name',
         surname: '$_id.surname',
         bio: '$_id.bio',
         movies: '$movies'
@@ -61,5 +63,58 @@ router.get('/', (req, res) => {
     res.json(err)
   })
 })
+
+router.get('/:director_id', (req, res) => {
+  const promise = Director.aggregate([
+    {
+      $match:{
+        '_id': mongoose.Types.ObjectId(req.params.director_id)
+      }
+    },
+    {
+      $lookup: {
+        from: 'movies',
+        localField: '_id',
+        foreignField: 'director_id',
+        as: 'movies'
+      }
+    },
+    {
+      $unwind: {
+        path: '$movies',
+        preserveNullAndEmptyArrays: true
+      }
+    },
+    {
+      $group: {
+        _id: {
+          _id: '$_id',
+          name: '$name',
+          surname: '$surname',
+          bio: '$bio'
+        },
+        movies: {
+          $push: '$movies'
+        }
+      }
+    },
+    {
+      $project: {
+        _id: '$_id._id',
+        name: '$_id.name',
+        surname: '$_id.surname',
+        bio: '$_id.bio',
+        movies: '$movies'
+      }
+    }
+  ])
+
+  promise.then((data) => {
+    res.json(data)
+  }).catch((err) => {
+    res.json(err)
+  })
+})
+
 
 module.exports = router;
