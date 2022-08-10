18e70fc53aa90baf5743cc90b67ec179a07965e3
alictnkaya8 <49043825+alictnkaya8@users.noreply.github.com>
2022-08-10 16:29:22 +0300

    movie and director models validations editted

--- models/Director.js
+++ models/Director.js
@@ -2,9 +2,21 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const DirectorSchema = new Schema({
-    name: String,
-    surname: String,
-    bio: String,
+    name: {
+        type: String,
+        maxlength: 50,
+        minlength: 2
+    },
+    surname: {
+        type: String,
+        maxlength: 50,
+        minlength: 2
+    },
+    bio: {
+        type: String,
+        maxlength: 200,
+        minlength: 1
+    },
     createdAt: {
         type: Date,
         default: Date.now
