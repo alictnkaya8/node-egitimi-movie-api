2a49930accaecc0e293b90446c8a8a5614bd4e9f
alictnkaya8 <49043825+alictnkaya8@users.noreply.github.com>
2022-08-12 09:19:35 +0300

    post movie and get movie by id tests added

--- helper/db.js
+++ helper/db.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 module.exports = () => {
     mongoose.connect("mongodb://localhost:27017/movie-api")
     mongoose.connection.on('open', () => {
-        console.log('MongoDB: Connected')
     })
     mongoose.connection.on('error', (err) => {
         console.log('MongoDB: Error', err)
